64013f63eaac6b7ffeb4a5b239ad1589cd17e1da
Josh Vince <joshmvince@gmail.com>
2021-12-04 10:36:19 +0000

    day3 part 1 refactor

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -12,24 +12,27 @@ class Day3(input: List<String>) {
         val leastCommonBits: MutableList<Int> = emptyList<Int>().toMutableList()
 
         for (i in 0 until bitLines.first().size) {
-            val occurrences = getOccurencesAtPos(i)
-            val mostCommonBit = occurrences.maxByOrNull { it.value }?.key ?: continue
-            val leastCommonBit = occurrences.minByOrNull { it.value }?.key ?: continue
+            val (leastCommonBit, mostCommonBit) = getBitByFrequency(i)
 
             mostCommonBits.add(mostCommonBit)
             leastCommonBits.add(leastCommonBit)
         }
 
-        val gammaRate = mostCommonBits.joinToString("").toInt(2)
-        val epsilonRate = leastCommonBits.joinToString("").toInt(2)
+        val gammaRate = convertToDecimal(mostCommonBits)
+        val epsilonRate = convertToDecimal(leastCommonBits)
 
         return gammaRate * epsilonRate
     }
 
-    private fun getOccurencesAtPos(pos: Int): Map<Int, Int> {
+    private fun getBitByFrequency(pos: Int): Pair<Int, Int> {
         val bitsAtPosition: List<Int> = bitLines.map { it[pos] }
-        return bitsAtPosition.groupingBy { it }.eachCount()
+        val (leastCommon, mostCommon) = bitsAtPosition.groupingBy { it }.eachCount()
+                .toList().sortedBy { (_bit, frequency) -> frequency }
+
+        return Pair(leastCommon.first, mostCommon.first)
     }
+
+    private fun convertToDecimal(bitArray: List<Int>): Int = bitArray.joinToString("").toInt(2)
 
     private fun convertToBits(line: String): List<Int> = line.map { it.digitToInt() }
 }
